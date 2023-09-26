cebb777a538f223e6fc53c3b4d6992dd6f0bf3ac
Oleg <scherbaoleg4@gmail.com>
2023-09-26 22:03:51 +0300

    catalog page & font & components (orientated for catalog only)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Navigate, Route, Routes} from "react-router-dom";
 
 import MainLayout from "./layouts/MainLayout/MainLayout";
 import HomePage from "./pages/HomePage/HomePage";
+import CatalogPage from './pages/CatalogPage/CatalogPage';
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <Route path={'/'} element={<MainLayout/>}>
                     <Route index element={<Navigate to={'home'}/>}/>
                     <Route path={'/home'} element={<HomePage/>}/>
+                    <Route path={'/catalog'} element={<CatalogPage/>}/>
                 </Route>
             </Routes>
         </div>
