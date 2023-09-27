ab1eb82814154acdbbb5cfdf3c17ccae7011ec5c
Oleg <scherbaoleg4@gmail.com>
2023-09-27 12:23:58 +0300

    transition

--- src/components/CatalogProductCard/CatalogProductCardComponent.js
+++ src/components/CatalogProductCard/CatalogProductCardComponent.js
@@ -14,20 +14,14 @@ const CatalogProductCard = () => {
                 <hr className={css.catalog_product_hr}/>
                 <h4 className={css.catalog_product_big_text}>Бумага</h4>
             </div>
-            <div className={css.catalog_product_open_panel}>
-            {
-                open && (
-                    <>
-                        <ScrollButtons/>
-                        <div className={css.catalog_product_container}>
-                            <ProductPreview/>
-                            <ProductPreview/>
-                            <ProductPreview/>
-                        </div>
-                        <ProductBtnMore/>
-                    </>
-                )
-            }
+            <div className={css.catalog_product_open_panel} style={open ? {maxHeight: 9999} : {maxHeight: 0}}>
+                <ScrollButtons/>
+                <div className={css.catalog_product_container}>
+                    <ProductPreview/>
+                    <ProductPreview/>
+                    <ProductPreview/>
+                </div>
+                <ProductBtnMore/>
             </div>
         </>
     );
