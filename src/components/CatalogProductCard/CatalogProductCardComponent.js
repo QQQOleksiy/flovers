08248b58d8f58990c7b575a700bce3fc268b5976
Oleg <scherbaoleg4@gmail.com>
2023-09-27 12:15:48 +0300

    card & improvements

---
+++ src/components/CatalogProductCard/CatalogProductCardComponent.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useState } from 'react';
+
+import css from './CatalogProductCard.module.css'
+
+import { ScrollButtons, ProductPreview, ProductBtnMore } from './../index'
+
+const CatalogProductCard = () => {
+    const [open, setOpen] = useState(false);
+
+    return (
+        <>
+            <div onClick={() => setOpen(!open)}>
+                <hr className={css.catalog_product_hr}/>
+                <h4 className={css.catalog_product_big_text}>Бумага</h4>
+            </div>
+            <div className={css.catalog_product_open_panel}>
+            {
+                open && (
+                    <>
+                        <ScrollButtons/>
+                        <div className={css.catalog_product_container}>
+                            <ProductPreview/>
+                            <ProductPreview/>
+                            <ProductPreview/>
+                        </div>
+                        <ProductBtnMore/>
+                    </>
+                )
+            }
+            </div>
+        </>
+    );
+};
+
+export {CatalogProductCard};
--- src/pages/CatalogPage/CatalogPage.js
+++ src/pages/CatalogPage/CatalogPage.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import { ScrollButtons, ProductPreview, ProductBtnMore } from './../../components/index'
+import { CatalogProductCard } from './../../components/index'
 
 import css from './catalog_page_style.module.css'
 
 const CatalogPage = () => {
     return (
         <div className={css.catalog_container}>
-            <hr className={css.catalog_hr}/>
-            <h4 className={css.catalog_big_text}>Бумага</h4>
-            {/* Можно в отдельный компонент запихнуть */}
-            <div>
-                <ScrollButtons/>
-                <div className={css.catalog_product_container}>
-                    <ProductPreview/>
-                    <ProductPreview/>
-                    <ProductPreview/>
-                </div>
-                <ProductBtnMore/>
-            </div>
-            <hr className={css.catalog_hr}/>
-            <h4 className={css.catalog_big_text}>Ленты</h4>
+            <CatalogProductCard/>
+            <CatalogProductCard/>
+            <CatalogProductCard/>
         </div>
     );
 };
