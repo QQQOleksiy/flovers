3b137a7c60eb204eafef0b929f2902539b238789
QQQOleksiy <oleksijslota@gmail.com>
2023-10-25 22:37:53 +0300

    Фіксаю проблемки

--- src/components/BurgerMenuComponent/BurgerMenuComponent.js
+++ src/components/BurgerMenuComponent/BurgerMenuComponent.js
@@ -1,34 +1,35 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {useNavigate} from "react-router-dom";
 
 import css from './burger_menu.module.css'
 import {flowerAction} from "../../redux";
 import MenuOnePositionComponent from "../MenuOnePositionComponent/MenuOnePositionComponent";
+import {useClickOutside} from "../../hooks/useClickOutside";
 
 const BurgerMenuComponent = () => {
-
-    let navigate = useNavigate();
+    let menuRef = useRef(null);
 
     const dispatch = useDispatch();
+    const { category, burger_menu} = useSelector(state => state.flowerReducer);
 
-    const {category} = useSelector(state => state.flowerReducer);
+    useClickOutside(menuRef, () => dispatch(flowerAction.close_menu()))
+
+    let navigate = useNavigate();
 
     useEffect(() =>{
         dispatch(flowerAction.getCategory())
     }, [dispatch])
 
-    const {burger_menu} = useSelector(state => state.flowerReducer);
-
     const scrollToBottom = () => {
         window.scrollTo({
-            top: document.documentElement.scrollHeight, // Прокручує до кінця сторінки
-            behavior: 'smooth', // Додає плавний ефект
+            top: document.documentElement.scrollHeight,
+            behavior: 'smooth',
         });
     };
 
     return (
-        <div className={burger_menu ? css.burger_menu_container_open : css.burger_menu_container_close}>
+        <div className={burger_menu ? css.burger_menu_container_open : css.burger_menu_container_close} ref={menuRef}>
             <div className={css.navigator}>
                 <div className={css.product_text}>
                     <div>Товары</div>
