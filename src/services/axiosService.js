bc5dc0c2035d03c45d9cb0056850ace6e1a10529
QQQOleksiy <oleksijslota@gmail.com>
2023-10-30 19:31:44 +0200

    Два варіанти запитів

--- src/services/axiosService.js
+++ src/services/axiosService.js
@@ -8,3 +8,19 @@ const apiService = axios.create({baseURL})
 export {
     apiService
 }
+
+// import axios from "axios";
+// import { baseURL } from "../config";
+//
+// const apiService = axios.create({baseURL});
+//
+// // Додаємо перехопник для обробки CORS-заголовків
+// apiService.interceptors.request.use(config => {
+//     // Додаємо заголовки для CORS
+//     config.headers["Access-Control-Allow-Origin"] = "*";
+//     // Додайте інші необхідні заголовки тут
+//
+//     return config;
+// });
+//
+// export { apiService };
